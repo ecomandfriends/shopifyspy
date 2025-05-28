819fa8aac2a57316147190c34fcc36be9f624550
ecomandfriends <toni@ecomandfriends.com>
2025-05-29 00:05:12 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def analyze():
         return jsonify({"error": f"No se pudo analizar la tienda: {str(e)}"})
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
